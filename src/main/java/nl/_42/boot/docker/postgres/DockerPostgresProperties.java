a4c1070b5952c27881d3c313fddbfc29aded4724
robert-bor <bor.robert@gmail.com>
2017-01-23 09:48:58 +0100

    First attempt to split projects into a specific Docker auto-configuration starter project (https://github.com/42BV/spring-boot-starter-docker) and a specific docker postgres project (this one, https://github.com/42BV/spring-boot-docker-postgres).

---
+++ src/main/java/nl/_42/boot/docker/postgres/DockerPostgresProperties.java
@@ -0,0 +1,125 @@
+package nl._42.boot.docker.postgres;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ConfigurationProperties(prefix = "docker.postgres", ignoreUnknownFields = false)
+public class DockerPostgresProperties {
+
+    private boolean enabled = false;
+
+    private String stdOutFilename = "docker-std-out.log";
+
+    private String stdErrFilename = "docker-std-err.log";
+
+    private String password = "postgres";
+
+    private Integer port = 5432;
+
+    private String imageName = "postgres";
+
+    private String imageVersion = "latest";
+
+    private String containerName = "postgression";
+
+    private String startupVerificationText = "PostgreSQL init process complete; ready for start up.";
+
+    private Integer timeout = 300000; // 5 minutes because of time required for downloading?
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getStdOutFilename() {
+        return stdOutFilename;
+    }
+
+    public void setStdOutFilename(String stdOutFilename) {
+        this.stdOutFilename = stdOutFilename;
+    }
+
+    public String getStdErrFilename() {
+        return stdErrFilename;
+    }
+
+    public void setStdErrFilename(String stdErrFilename) {
+        this.stdErrFilename = stdErrFilename;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public String getContainerName() {
+        return containerName;
+    }
+
+    public void setContainerName(String containerName) {
+        this.containerName = containerName;
+    }
+
+    public String getImageVersion() {
+        return imageVersion;
+    }
+
+    public void setImageVersion(String imageVersion) {
+        this.imageVersion = imageVersion;
+    }
+
+    public Integer getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(Integer timeout) {
+        this.timeout = timeout;
+    }
+
+    public String getStartupVerificationText() {
+        return startupVerificationText;
+    }
+
+    public void setStartupVerificationText(String startupVerificationText) {
+        this.startupVerificationText = startupVerificationText;
+    }
+
+    public Map<String, String> getProperties() {
+        Map<String,String> properties = new HashMap<>();
+        properties.put("stdOutFilename", getStdOutFilename());
+        properties.put("stdErrFilename", getStdErrFilename());
+        properties.put("timeout", getTimeout().toString());
+        properties.put("password", getPassword());
+        properties.put("port", getPort().toString());
+        properties.put("containerName", getContainerName());
+        properties.put("imageName", getImageName());
+        properties.put("imageVersion", getImageVersion());
+        properties.put("startupVerificationText", getStartupVerificationText());
+        return properties;
+    }
+
+}
---
+++ src/main/java/nl/_42/boot/docker/postgres/DockerPostgresTailer.java
@@ -0,0 +1,111 @@
+package nl._42.boot.docker.postgres;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+
+public class DockerPostgresTailer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DockerPostgresTailer.class);
+
+    private final Thread dockerThread;
+    private final String dockerStandardOutFilename;
+    private final String dockerStandardErrorFilename;
+    private final String startupVerificationText;
+    private final Integer timeout;
+
+    private Integer sleepTime = 0;
+
+    public DockerPostgresTailer(Thread dockerThread,
+                                String dockerStandardOutFilename,
+                                String dockerStandardErrorFilename,
+                                String startupVerificationText,
+                                Integer timeout) {
+        this.dockerThread = dockerThread;
+        this.dockerStandardOutFilename = dockerStandardOutFilename;
+        this.dockerStandardErrorFilename = dockerStandardErrorFilename;
+        this.startupVerificationText = startupVerificationText;
+        this.timeout = timeout;
+    }
+
+    public boolean verify() throws IOException {
+
+        final BufferedInputStream reader;
+        try {
+            reader = new BufferedInputStream(new FileInputStream( dockerStandardOutFilename) );
+        } catch (FileNotFoundException e) {
+            throw e;
+        }
+
+        try {
+            StringBuilder line = new StringBuilder();
+            while(true) {
+                if( reader.available() > 0 ) {
+                    char readChar = (char)reader.read();
+                    if (readChar == '\n') {
+                        if (line.toString().contains(startupVerificationText)) {
+                            LOGGER.info("| > " + line.toString());
+                            LOGGER.info("| = Docker startup verification text found");
+                            logErrorLinesAsWarning();
+                            return true;
+                        }
+                        LOGGER.info("| > " + line.toString());
+                        line = new StringBuilder();
+                    } else {
+                        line.append(readChar);
+                    }
+                }
+                else {
+                    try {
+                        if (sleepTime > timeout) {
+                            LOGGER.error("| = Startup could not be verified. Interrupting process. <check your verification string>");
+                            dockerThread.interrupt();
+                        }
+
+                        sleepTime += 100;
+                        Thread.sleep( 100 );
+
+                        if (!dockerThread.isAlive()) {
+                            LOGGER.error("| = Docker Postgres container has stopped processing");
+                            logErrorLinesAsError();
+                            return false;
+                        }
+                    }
+                    catch( InterruptedException ex ) {
+                        LOGGER.error("| = Docker Postgres container failed to initialize");
+                        return false;
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            reader.close();
+        }
+
+        LOGGER.error("| = Docker Postgres container failed to initialize");
+        return false;
+    }
+
+    private void logErrorLinesAsError() throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(dockerStandardErrorFilename)))) {
+            String errLine;
+            while ((errLine = br.readLine()) != null) {
+                LOGGER.error("| > " + errLine);
+            }
+        }
+    }
+
+    private void logErrorLinesAsWarning() throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(dockerStandardErrorFilename)))) {
+            String errLine;
+            while ((errLine = br.readLine()) != null) {
+                LOGGER.warn("| > " + errLine);
+            }
+        }
+    }
+
+}
